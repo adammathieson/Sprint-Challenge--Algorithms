2f80a607d1ca28e7c35e624968dcd43f0aa5be80
Adam Mathieson <mathieson1222@gmail.com>
2019-10-11 09:17:05 -0700

    test will pass individully, but when run consecutive they overwrite the cache, need to keep count of each function

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,19 @@ Your function should take in a signle parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+cache = []
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    if len(word) < 2:
+        return
+    elif word[0:2] == 'th':
+        cache.append(word[0:2])
+    split_word = word[1:]
+    count_th(split_word)
+
+    return len(cache)
+
+# print(count_th('')) #None
+# print(count_th('abcthxyz')) #1
+# print(count_th("abcthefthghith")) #3
+# print(count_th("THtHThth")) #1
